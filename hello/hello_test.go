ec4964de09f0658371ab4ff748025925c05cac1e
syashakash <akashshivram9@gmail.com>
2020-04-20 17:42:17 +0800

    EDIT: Reafctor test file club testcases in one func. Club testcases under one func to consolidate testing for one file

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -3,20 +3,21 @@ package main
 import "testing"
 
 func TestSayHelloCustom(t *testing.T)  {
-	expected := "Hello, Akash!!"
-	got := sayHelloCustom("Akash")
+	t.Run("saying hello to people", func(t *testing.T) {
+		expected := "Hello, Akash!!"
+		got := sayHelloCustom("Akash")
 
-	if got != expected {
-		t.Errorf("Got %q, while expected %q", got, expected)
-	}
-}
+		if got != expected {
+			t.Errorf("Got %q, while expected %q", got, expected)
+		}
+	})
 
-func TestSayHelloWorld(t *testing.T) {
+	t.Run("say 'Hello, World!!'", func(t *testing.T) {
+		expected := "Hello, World!!"
+		got := sayHelloWorld()
 
-	expected := "Hello, World!!"
-	got := sayHelloWorld()
-
-	if got != expected {
-		t.Errorf("Got %q, while expected %q", got, expected)
-	}
+		if got != expected {
+			t.Errorf("Got %q, while expected %q", got, expected)
+		}
+	})
 }
