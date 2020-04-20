581d1ce523702980f729bc67e4890439b07e81f8
syashakash <akashshivram9@gmail.com>
2020-04-21 01:33:01 +0800

    EDIT: Refactor code to suit the methods Area for Rectangle and Circle structs

--- struct_methods_interfaces/geometry_test.go
+++ struct_methods_interfaces/geometry_test.go
@@ -13,11 +13,23 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	rectangle := Rectangle{9.00, 6.00}
-	got := Area(rectangle)
-	want := 54.00
+	t.Run("rectangles", func(t *testing.T) {
+		rectangle := Rectangle{12, 6}
+		got := rectangle.Area()
+		want := 72.0
 
-	if got != want {
-		t.Errorf("got %.2f, expected %.2f", got, want)
-	}
+		if got != want {
+			t.Errorf("got %g want %g", got, want)
+		}
+	})
+
+	t.Run("circles", func(t *testing.T) {
+		circle := Circle{10}
+		got := circle.Area()
+		want := 314.1592653589793
+
+		if got != want {
+			t.Errorf("got %g want %g", got, want)
+		}
+	})
 }
