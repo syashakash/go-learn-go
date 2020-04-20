79c29104dd1c748d69a7f06347aeb3aed129cc77
syashakash <akashshivram9@gmail.com>
2020-04-20 18:31:06 +0800

    ADD: Test file to test Add method

--- integers/adder_test.go
+++ integers/adder_test.go
@@ -1,1 +1,12 @@
 package integers
+
+import "testing"
+
+func TestAdder(t *testing.T) {
+	sum := Add(100, 25)
+	expected := 125
+
+	if sum != expected {
+		t.Errorf("expected '%d' but got '%d'", expected, sum)
+	}
+}
