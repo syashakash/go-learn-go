04e7684b25a430366793600ed1f284362bc2cb0b
syashakash <akashshivram9@gmail.com>
2020-04-20 17:54:56 +0800

    EDIT: Refactor code to introduce helper for testcases that result same output The check for expected and got were same in the two subtests, to reduce duplicacy and improve readibility add func assertCorrectMessage that returns output as desired by subtests

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -3,21 +3,23 @@ package main
 import "testing"
 
 func TestSayHelloCustom(t *testing.T)  {
-	t.Run("saying hello to people", func(t *testing.T) {
-		expected := "Hello, Akash!!"
-		got := sayHelloCustom("Akash")
 
+	assertCorrectMessage := func(t *testing.T, got, expected string) {
+		t.Helper()
 		if got != expected {
 			t.Errorf("Got %q, while expected %q", got, expected)
 		}
+	}
+
+	t.Run("saying hello to people", func(t *testing.T) {
+		expected := "Hello, Akash!!"
+		got := sayHello("Akash")
+		assertCorrectMessage(t, got, expected)
 	})
 
 	t.Run("say 'Hello, World!!'", func(t *testing.T) {
 		expected := "Hello, World!!"
-		got := sayHelloWorld()
-
-		if got != expected {
-			t.Errorf("Got %q, while expected %q", got, expected)
-		}
+		got := sayHello("")
+		assertCorrectMessage(t, got, expected)
 	})
 }
