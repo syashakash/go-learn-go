d79b32142602a86c2e3bcf180692607fa5af8bba
syashakash <akashshivram9@gmail.com>
2020-04-21 01:14:51 +0800

    EDIT: Refactor to use Rectangle struct instead of primitive type

--- struct_methods_interfaces/geometry_test.go
+++ struct_methods_interfaces/geometry_test.go
@@ -3,7 +3,8 @@ package struct_methods_interfaces
 import "testing"
 
 func TestPerimeter(t *testing.T) {
-	got := Perimeter(10.00, 8.00)
+	rectangle := Rectangle{10.00, 8.00}
+	got := Perimeter(rectangle)
 	want := 36.00
 
 	if got != want {
@@ -12,8 +13,10 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	got := Area(9.0, 6.0)
+	rectangle := Rectangle{9.00, 6.00}
+	got := Area(rectangle)
 	want := 54.00
+
 	if got != want {
 		t.Errorf("got %.2f, expected %.2f", got, want)
 	}
