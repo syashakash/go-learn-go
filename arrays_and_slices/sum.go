cd87076891c824e0c6a8c7bf07073dfbf9ed91ca
syashakash <akashshivram9@gmail.com>
2020-04-21 00:01:13 +0800

    EDIT: Add condition to check size of slize before getting sum

--- arrays_and_slices/sum.go
+++ arrays_and_slices/sum.go
@@ -18,3 +18,16 @@ func SumAll(numbersToSum ...[]int) (sums []int){
 
 	return
 }
+
+func SumAllTails(numbersToSumOfTails ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSumOfTails {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		}
+	}
+	return sums
+}
