3418aac207530b7f62167a5391a2e54d29f134f3
syashakash <akashshivram9@gmail.com>
2020-04-20 18:16:43 +0800

    ADD: New private function greetingPrefix and refactor method SayHello to include language as a param

--- hello/hello.go
+++ hello/hello.go
@@ -3,14 +3,30 @@ package main
 import "fmt"
 
 const englishHelloPrefix = "Hello, "
+const spanish = "ES"
+const english = "EN"
+const french = "FR"
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
 
-func sayHello(name string) string {
+func SayHello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
-	return englishHelloPrefix + name + "!!"
+	return greetingPrefix(language) + name + "!!"
 }
 
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case french:
+		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
+}
 func main() {
-	fmt.Println(sayHello(""))
+	fmt.Println(sayHello("", "ES"))
 }
