8270226de4c9e605379b1ff7ec13251590ed928e
syashakash <akashshivram9@gmail.com>
2020-04-20 18:35:53 +0800

    EDIT: Refactor: Reaname Test func

--- integers/adder_test.go
+++ integers/adder_test.go
@@ -1,12 +1,21 @@
 package integers
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
-func TestAdder(t *testing.T) {
+func TestAdd(t *testing.T) {
 	sum := Add(100, 25)
 	expected := 125
 
 	if sum != expected {
 		t.Errorf("expected '%d' but got '%d'", expected, sum)
 	}
+}
+
+func ExampleAdd() {
+	sum := Add(1, 5)
+	fmt.Println(sum)
+	// Output: 6
 }
