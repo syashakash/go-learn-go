4a3dbef387d4bd7034a8cd8c74734920f0c05836
syashakash <akashshivram9@gmail.com>
2020-04-20 22:23:24 +0800

    ADD: test case on sum of array elements

--- arrays_and_slices/sum_test.go
+++ arrays_and_slices/sum_test.go
@@ -1,1 +1,14 @@
 package arrays_and_slices
+
+import "testing"
+
+func TestSum(t *testing.T) {
+	numbers := [5]int{1, 2, 3, 4, 5}
+
+	got := Sum(numbers)
+	want := 15
+
+	if got != want {
+		t.Errorf("got %d want %d, %v", got, want, numbers)
+	}
+}
