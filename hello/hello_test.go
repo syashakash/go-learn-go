d3dd24e449de64fab1f3d86cc8da99654f19f85e
syashakash <akashshivram9@gmail.com>
2020-04-20 18:10:02 +0800

    ADD: Language as new argument

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -13,13 +13,25 @@ func TestSayHelloCustom(t *testing.T)  {
 
 	t.Run("saying hello to people", func(t *testing.T) {
 		expected := "Hello, Akash!!"
-		got := sayHello("Akash")
+		got := sayHello("Akash", "EN")
 		assertCorrectMessage(t, got, expected)
 	})
 
 	t.Run("say 'Hello, World!!'", func(t *testing.T) {
 		expected := "Hello, World!!"
-		got := sayHello("")
+		got := sayHello("", "EN")
+		assertCorrectMessage(t, got, expected)
+	})
+
+	t.Run("in Spanish", func(t *testing.T) {
+		expected := "Hola, Elodie!!"
+		got := sayHello("Elodie", "ES")
+		assertCorrectMessage(t, got, expected)
+	})
+
+	t.Run("in French",func(t *testing.T) {
+		expected := "Bonjour, Elodie!!"
+		got := sayHello("Elodie", "FR")
 		assertCorrectMessage(t, got, expected)
 	})
 }
