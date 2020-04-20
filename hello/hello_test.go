c5256c61258716cc837f78df1c9f919ec5fc44fb
syashakash <akashshivram9@gmail.com>
2020-04-20 17:34:29 +0800

    Add new testcase for testing sayHelloCustom

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -2,9 +2,19 @@ package main
 
 import "testing"
 
-func  TestSayHello(t *testing.T)  {
+func TestSayHelloCustom(t *testing.T)  {
+	expected := "Hello, Akash!!"
+	got := sayHelloCustom("Akash")
+
+	if got != expected {
+		t.Errorf("Got %q, while expected %q", got, expected)
+	}
+}
+
+func TestSayHelloWorld(t *testing.T) {
+
 	expected := "Hello, World!!"
-	got := sayHello()
+	got := sayHelloWorld()
 
 	if got != expected {
 		t.Errorf("Got %q, while expected %q", got, expected)
