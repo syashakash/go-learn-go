dee4c71ce20a6414ce0de7a046f2a62fea369c49
syashakash <akashshivram9@gmail.com>
2020-04-20 17:34:06 +0800

    ADD: New fucntion for custom output and refactor name of sayHello to sayHelloWorld

--- hello/hello.go
+++ hello/hello.go
@@ -2,9 +2,15 @@ package main
 
 import "fmt"
 
-func sayHello() string {
+func sayHelloWorld() string {
 	return "Hello, World!!"
 }
+
+func sayHelloCustom(name string) string {
+	return "Hello, " + name + "!!"
+}
+
 func main() {
-	fmt.Println(sayHello())
+	fmt.Println(sayHelloWorld())
+	fmt.Println(sayHelloCustom("Akash"))
 }
