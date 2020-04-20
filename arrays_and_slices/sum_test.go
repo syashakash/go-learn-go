f9a1a8d5d87172c32fa71a60dedc12155dbed433
syashakash <akashshivram9@gmail.com>
2020-04-20 22:51:00 +0800

    ADD: Helper function and testcase independent of size

--- arrays_and_slices/sum_test.go
+++ arrays_and_slices/sum_test.go
@@ -3,12 +3,19 @@ package arrays_and_slices
 import "testing"
 
 func TestSum(t *testing.T) {
-	numbers := [5]int{1, 2, 3, 4, 5}
-
-	got := Sum(numbers)
-	want := 15
-
-	if got != want {
-		t.Errorf("got %d want %d, %v", got, want, numbers)
+	assertCorrectAnswer := func(t *testing.T, got, want int, numbers []int) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %d want %d, %v", got, want, numbers)
+		}
 	}
+
+	t.Run("collection of any size", func(t *testing.T) {
+		numbers := []int{1, 2, 3, 4, 5}
+
+		got := Sum(numbers)
+		want := 15
+		assertCorrectAnswer(t, got, want, numbers)
+	})
+
 }
