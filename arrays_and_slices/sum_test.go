097939b8962c7b2da13c5c271dc6842f7de88a1b
syashakash <akashshivram9@gmail.com>
2020-04-20 22:51:31 +0800

    EDIT: Refactor func to take slices as params as well

--- arrays_and_slices/sum_test.go
+++ arrays_and_slices/sum_test.go
@@ -9,6 +9,13 @@ func TestSum(t *testing.T) {
 			t.Errorf("got %d want %d, %v", got, want, numbers)
 		}
 	}
+	t.Run("collection of 5 numbers", func(t *testing.T) {
+		numbers := []int{1, 2, 3, 4, 5}
+
+		got := Sum(numbers)
+		want := 15
+		assertCorrectAnswer(t, got, want, numbers)
+	})
 
 	t.Run("collection of any size", func(t *testing.T) {
 		numbers := []int{1, 2, 3, 4, 5}
@@ -17,5 +24,4 @@ func TestSum(t *testing.T) {
 		want := 15
 		assertCorrectAnswer(t, got, want, numbers)
 	})
-
 }
