fc84a1089c3e535755561d7d175aff292a483b56
syashakash <akashshivram9@gmail.com>
2020-04-21 16:02:29 +0800

    ADD: testcase to check update of non-existing word

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -46,15 +46,29 @@ func TestAdd(t *testing.T) {
 }
 
 func TestUpdate(t *testing.T)  {
-	word := "test"
-	definition := "this is just a test"
-	dictionary := Dictionary{word: definition}
-	newDefinition := "new definition"
+	t.Run("existing word", func(t * testing.T) {
+		word := "test"
+		definition := "this is just a test"
+		dictionary := Dictionary{word: definition}
+		newDefinition := "new definition"
 
-	dictionary.Update(word, newDefinition)
+		dictionary.Update(word, newDefinition)
 
-	assertDefinition(t, dictionary, word, newDefinition)
+		assertDefinition(t, dictionary, word, newDefinition)
+	})
+
+	t.Run("new word", func(t *testing.T) {
+		word := "test"
+		definition := "this is just a test"
+		dictionary := Dictionary{}
+
+		err := dictionary.Update(word, definition)
+
+		assertError(t, err, ErrWordDoesNotExists)
+	})
 }
+
+
 func assertStrings(t *testing.T, got, want string) {
 	t.Helper()
 
