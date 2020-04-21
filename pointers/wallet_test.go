1267353f2a4f04b5b845d8d27080783ed9c8783a
syashakash <akashshivram9@gmail.com>
2020-04-21 11:57:40 +0800

    ADD: subtest to check for insufficient funds

--- pointers/wallet_test.go
+++ pointers/wallet_test.go
@@ -5,13 +5,37 @@ import (
 )
 
 func TestWallet(t *testing.T) {
-	wallet := Wallet{}
-	wallet.Deposit(Bitcoin(30))
-
-	got := wallet.Balance()
-	want:= Bitcoin(30)
-	// fmt.Printf("address of balance in test is %v \n", &wallet.balance)
-	if got != want {
-		t.Errorf("got %s, want %s", got, want)
+	assertBalance := func(t *testing.T, wallet Wallet, want Bitcoin) {
+		t.Helper()
+		got := wallet.Balance()
+		if got != want {
+			t.Errorf("got %s, want %s", got, want)
+		}
 	}
+
+	t.Run("Deposit", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(Bitcoin(30))
+		want := Bitcoin(30)
+		assertBalance(t, wallet, want)
+	})
+
+	t.Run("Withdraw", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(30)}
+		wallet.Withdraw(Bitcoin(10))
+		want := Bitcoin(20)
+		assertBalance(t, wallet, want)
+	})
+
+	t.Run("Withdraw insufficient funds", func(t *testing.T) {
+		startingBalance :=  Bitcoin(30)
+		wallet := Wallet{balance:startingBalance}
+		err := wallet.Withdraw(Bitcoin(100))
+
+		assertBalance(t, wallet, startingBalance)
+
+		if err == nil {
+			t.Errorf("Wanted an error but didn't get one")
+		}
+	})
 }
