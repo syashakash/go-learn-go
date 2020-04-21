c72304047547b00d1b5083db5845267f9a857907
syashakash <akashshivram9@gmail.com>
2020-04-21 14:35:18 +0800

    ADD: custom error for key not found

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -1,7 +1,15 @@
 package maps
 
+import "errors"
+
 type Dictionary map[string]string
 
-func (dict Dictionary) Search(word string) string {
-	return dict[word]
+var ErrNotFound = errors.New("could not find the word you were looking for")
+
+func (dict Dictionary) Search(word string) (string, error) {
+	definition, ok := dict[word]
+	if !ok {
+		return "", ErrNotFound
+	}
+	return definition, nil
 }
