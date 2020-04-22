8bd795a0b120b2e8922e3091cd00d23a5b94f408
syashakash <akashshivram9@gmail.com>
2020-04-23 01:11:17 +0800

    ADD: ConfigurableSleeper

--- mocking/countdown.go
+++ mocking/countdown.go
@@ -24,13 +24,13 @@ func (s *SpySleeper) Sleep() {
 	s.Calls++
 }
 
-
+/*
 type DefaultSleeper struct {}
 
 func (d *DefaultSleeper) Sleep() {
 	time.Sleep(1 * time.Second)
 }
-
+*/
 
 type CountdownOperationsSpy struct {
 	Calls []string
@@ -43,6 +43,24 @@ func (s *CountdownOperationsSpy) Sleep() {
 func (s *CountdownOperationsSpy) Write(p []byte) (n int, err error) {
 	s.Calls = append(s.Calls, write)
 	return
+}
+
+type ConfigurableSleeper struct {
+	duration time.Duration
+	sleep func(time.Duration)
+}
+
+func (c *ConfigurableSleeper) Sleep() {
+	c.sleep(c.duration)
+}
+
+
+type SpyTime struct {
+	durationSlept time.Duration
+}
+
+func (s *SpyTime) Sleep(duration time.Duration) {
+	s.durationSlept = duration
 }
 
 
@@ -58,7 +76,8 @@ func Countdown(out io.Writer, sleeper Sleeper) {
 
 /*
 func main() {
-	sleeper := &DefaultSleeper{}
+	// sleeper := &DefaultSleeper{}
+	sleeper := &ConfigurableSleeper{1 * time.Second, time.Sleep}
 	Countdown(os.Stdout, sleeper)
 }
  */
