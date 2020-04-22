4b6827c88a714ad205899eccfee23bdad4071bdc
syashakash <akashshivram9@gmail.com>
2020-04-23 00:35:00 +0800

    ADD: Sleeper SpySleeper and DefaultSleeper to mock time.Sleep

--- mocking/countdown.go
+++ mocking/countdown.go
@@ -3,20 +3,45 @@ package mocking
 import (
 	"fmt"
 	"io"
+	"time"
 )
 
-const countdownStart = 3
+type Sleeper interface {
+	Sleep()
+}
 
-func Countdown(out io.Writer) {
+type SpySleeper struct {
+	Calls int
+}
+
+func (s *SpySleeper) Sleep() {
+	s.Calls++
+}
+
+
+type DefaultSleeper struct {}
+
+func (d *DefaultSleeper) Sleep() {
+	time.Sleep(1 * time.Second)
+}
+
+
+const countdownStart = 3
+const finalWord = "Go!"
+
+func Countdown(out io.Writer, sleeper Sleeper) {
 	//fmt.Fprint(out, "3")
 	for i := countdownStart; i > 0; i-- {
+		sleeper.Sleep()
 		fmt.Fprintln(out, i)
 	}
-	fmt.Fprint(out, "Go!")
+	sleeper.Sleep()
+	fmt.Fprint(out, finalWord)
 }
 
 /*
 func main() {
-	Countdown(os.Stdout)
+	sleeper := &DefaultSleeper{}
+	Countdown(os.Stdout, sleeper)
 }
  */
