5789262ebefab979dabe81ec1d0001c9da70fbea
syashakash <akashshivram9@gmail.com>
2020-04-22 23:54:40 +0800

    EDIT: Change output string

--- mocking/countdown.go
+++ mocking/countdown.go
@@ -5,8 +5,14 @@ import (
 	"io"
 )
 
+const countdownStart = 3
+
 func Countdown(out io.Writer) {
-	fmt.Fprint(out, "3")
+	//fmt.Fprint(out, "3")
+	for i := countdownStart; i > 0; i-- {
+		fmt.Fprintln(out, i)
+	}
+	fmt.Fprint(out, "Go!")
 }
 
 /*
