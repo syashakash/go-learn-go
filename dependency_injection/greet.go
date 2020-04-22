88a548a5b87408587e364518015992df99a70be9
syashakash <akashshivram9@gmail.com>
2020-04-22 23:22:22 +0800

    ADD: commented MyFreetHandler to show where else we can write output

--- dependency_injection/greet.go
+++ dependency_injection/greet.go
@@ -1,16 +1,22 @@
 package dependency_injection
 
 import (
-	"bytes"
 	"fmt"
 	"io"
-	"os"
 )
 
 func Greet(writer io.Writer, name string) {
 	fmt.Fprintf(writer,"Hello, %s", name)
 }
 
+/*
 func main() {
-	Greet(os.Stdout, "Elodie")
+	//Greet(os.Stdout, "Elodie")
+	http.ListenAndServe(":5000", http.HandlerFunc(MyGreetHandler))
 }
+
+func MyGreetHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(r)
+	Greet(w, "world")
+}
+*/
