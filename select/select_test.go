f0f41270aff579dd58ee7a3f2b5e465e0e1d3d63
syashakash <akashshivram9@gmail.com>
2020-05-03 23:59:54 +0800

    Refactor into subtests

--- select/select_test.go
+++ select/select_test.go
@@ -7,20 +7,19 @@ import (
 )
 
 func TestRacer(t *testing.T)  {
-	slowServer := makeDelayedServer(20 * time.Millisecond)
-	fastServer := makeDelayedServer(0 * time.Millisecond)
+	t.Run("returns an error if a server doesn't respond within 10s", func(t *testing.T){
+		serverA := makeDelayedServer(11 * time.Millisecond)
+		serverB := makeDelayedServer(12 * time.Millisecond)
+		defer serverA.Close()
+		defer serverB.Close()
 
-	slowURL := slowServer.URL
-	fastURL := fastServer.URL
+		_, err :=  Racer(serverA.URL, serverB.URL)
+		if err == nil {
+			t.Errorf("expected an error but didn't get one")
+		}
+	})
 
-	want := fastURL
-	got :=  Racer(slowURL, fastURL)
-	if got != want {
-		t.Errorf("got %q, want %q", got, want)
-	}
 
-	slowServer.Close()
-	fastServer.Close()
 }
 
 func makeDelayedServer(delay time.Duration) *httptest.Server {
