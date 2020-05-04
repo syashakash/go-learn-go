e8e1dce5be66d2a03a1fddaada96b5c526803847
syashakash <akashshivram9@gmail.com>
2020-05-05 00:39:48 +0800

    ADD Store and Server funcs

--- context/server.go
+++ context/server.go
@@ -1,1 +1,17 @@
 package context
+
+import (
+	"fmt"
+	"net/http"
+)
+
+type Store interface {
+	Fetch() string
+}
+
+
+func Server(store Store) (http.HandlerFunc) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, store.Fetch())
+	}
+}
