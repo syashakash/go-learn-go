c51879a84aba5d567e395d3ff69b96bf82b3d6c2
syashakash <akashshivram9@gmail.com>
2020-05-05 01:12:03 +0800

    ADD store.Cancel to cancel sontext

--- context/server.go
+++ context/server.go
@@ -13,6 +13,7 @@ type Store interface {
 
 func Server(store Store) (http.HandlerFunc) {
 	return func(w http.ResponseWriter, r *http.Request) {
+		store.Cancel()
 		fmt.Fprint(w, store.Fetch())
 	}
 }
