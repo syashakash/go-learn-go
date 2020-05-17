2475a3bb9b0b1b21bf3a830436633c9300990e10
syashakash <akashshivram9@gmail.com>
2020-05-17 23:52:58 +0800

    Add test for ConvertToArabic

--- property_based_tests/roman_test.go
+++ property_based_tests/roman_test.go
@@ -1,6 +1,9 @@
 package property_based_tests
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestRomanNumerals(t *testing.T) {
 	cases := []struct{
@@ -44,6 +47,17 @@ func TestRomanNumerals(t *testing.T) {
 			got := ConvertToRoman(test.Arabic)
 			if got != test.Roman {
 				t.Errorf("got %q, want %q", got, test.Roman)
+			}
+		})
+	}
+}
+
+func TestConvertingToArabic(t *testing.T) {
+	for _, test := range cases[:1] {
+		t.Run(fmt.Sprintf("%q gets converted to %d", test.Roman, test.Arabic), func(t *testing.T) {
+			got := ConvertToArabic(test.Roman)
+			if got != test.Arabic {
+				t.Errorf("got %d, want %d", got, test.Arabic)
 			}
 		})
 	}
