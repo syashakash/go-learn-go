49b2a7a1740efad65fb5738bbab9ced1452cb60b
syashakash <akashshivram9@gmail.com>
2020-05-17 23:42:16 +0800

    Add roman numeral arrays

--- property_based_tests/roman.go
+++ property_based_tests/roman.go
@@ -7,6 +7,14 @@ type RomanNumeral struct {
 	Symbol string
 }
 
+var allRomanNumerals = []RomanNumeral {
+	{10, "X"},
+	{9, "IX"},
+	{5, "V"},
+	{4, "IV"},
+	{1, "I"},
+}
+
 func ConvertToRoman(arabic int) string {
 	var result strings.Builder
 
